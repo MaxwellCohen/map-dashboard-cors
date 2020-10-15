34c1a586aa2730f0095a63059dd53dc055c1db63
Maxwell Cohen <maxwell.cohen@globant.com>
2020-10-14 20:47:59 -0500

    remove day cache

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 const DEFAULT_PORT = 8080;
 const port = process.env.PORT || DEFAULT_PORT;
-const DAY = 24 * 60 * 60;
 
 corsProxy.createServer({
   // Allow all origins
   checkRateLimit: checkRateLimit('50 3 http://localhost:3000 https://maxwellcohen.github.io'),
-  corsMaxAge: DAY,
   originWhitelist: [
     'http://localhost:3000',
     'https://maxwellcohen.github.io'
